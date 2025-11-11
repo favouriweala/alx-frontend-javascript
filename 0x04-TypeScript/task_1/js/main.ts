ad264821b5bc172734d3d5b1c8a87c8e8688ab0a
favouriweala <favouriweala@gmail.com>
2025-11-11 17:06:45 +0100

    minor fix

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,3 @@
-// Teacher Interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -8,12 +7,14 @@ interface Teacher {
   [key: string]: any;
 }
 
-//Directors Interface extending Teacher
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// teacher
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -24,25 +25,14 @@ const teacher3: Teacher = {
 
 console.log(teacher3);
 
-// director
 const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
+  firstName: 'Jane',
+  lastName: 'Smith',
+  location: 'New York',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
 
 console.log(director1);
 
-// Interface for function printTeacher
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// Implementation of printTeacher
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-console.log(printTeacher("John", "Doe")); 
+console.log(printTeacher({ firstName: 'John', lastName: 'Doe' }));
